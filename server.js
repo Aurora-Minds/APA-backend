7722065d9184749242802fbb05c2e102fc591e31
Bibekhazard <bibekhazardpandey@gmail.com>
2025-06-20 05:34:30 -0400

    fix(cors): Restrict CORS to specific origin and enable credentials

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://www.auroraminds.xyz',
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB Connection
